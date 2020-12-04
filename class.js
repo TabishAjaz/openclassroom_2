926191d357e71a200115c3f48baf5406e28213e5
TABISH AJAZ <53833750+TabishAjaz@users.noreply.github.com>
2020-12-04 19:14:08 +0530

    Create class.js

---
+++ class.js
@@ -0,0 +1,47 @@
+// DOM ELEMENT REFERENCES
+const hobbiesResult = document.getElementById('hobbies-result');
+const transportResult = document.getElementById('transport-result');
+const musicResult = document.getElementById('music-result');
+const transportPreference=document.getElementsByName('transport-method');
+const musicPreference=document.getElementById('music-preference');
+ //default for better experience
+ musicResult.textContent=musicPreference.value;
+/*
+ * CHECKBOX LISTENERS
+ */
+document.getElementById('sports-checkbox').addEventListener('change', ($event) => {
+  if ($event.target.checked) {
+    hobbiesResult.children[0].classList.remove('text-secondary');
+  }
+  else {
+    hobbiesResult.children[0].classList.add('text-secondary');
+  }
+});
+
+document.getElementById('games-checkbox').addEventListener('change', ($event) => {
+  if ($event.target.checked) {
+    hobbiesResult.children[1].classList.remove('text-secondary');
+  }
+  else {
+    hobbiesResult.children[1].classList.add('text-secondary');
+  }
+});
+
+document.getElementById('music-checkbox').addEventListener('change', ($event) => {
+  if ($event.target.checked) {
+    hobbiesResult.children[2].classList.remove('text-secondary');
+  }
+  else {
+    hobbiesResult.children[2].classList.add('text-secondary');
+  }
+});
+
+for(let i=0;i< transportPreference.length;i++){
+  transportPreference[i].addEventListener('change', ($event)=>{
+    transportResult.textContent= $event.target.value;
+  });
+}
+
+musicPreference.addEventListener('change',($event)=>{
+  musicResult.textContent=$event.target.value;
+});
